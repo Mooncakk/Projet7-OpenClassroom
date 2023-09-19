13d55e01e97469c67a3ec44af2d92c19e3d70e47
Adm1n <adm1n@Kal1X.local>
2023-09-19 23:49:55 +0200

    update bruteforce & add Shares.csv

--- bruteforce.py
+++ bruteforce.py
@@ -1,49 +1,107 @@
-import json
-from decimal import *
-from operator import itemgetter
-from itertools import accumulate
+import pandas as pd
+from itertools import combinations
+import time
 
-def read_json(filename):
-    with open ("liste_actions.json", "r") as file:
-        data =  json.load(file)
-    return data
+def shares(data='./Shares.csv'):
+    '''Lis le fichier csv et créé un tuples contenant les infos des actions, les prix et le bénéfice apres 2 ans''' 
 
+    data = pd.read_csv(data, delimiter = ';')
+    shares_list = []
 
-share_list = read_json("stock_list.json")
-cost_netprofit_list = [] 
+    for row in data.itertuples():
+        share = row[1]
+        price = row[2]
+        efficiency = row[3]
+        profit = price * efficiency/100
+        share_infos = share, price, profit
+        shares_list.append(share_infos)
+               
+    return shares_list
 
-""" Faire une liste de tuples action/cout/profit_réel"""
-for i in range(1, len(share_list)+1):
-    cost = (share_list[f'{i}']['cost'])
-    profit = (share_list[f'{i}']['profit'])
-    net_profit = cost * (profit/100)
-    cost_netprofit_list.append((i, cost,net_profit))
+def combination_list(shares_list):
+    '''Compare toutes les combinantions possible entre les actions et enregistre chaque combinaison dans une liste'''
 
-#print(cost_netprofit_list)
+    combination_list = []
 
-""" Classer la liste par ordre décroissant selon les profits réel"""    
-cost_netprofit_sorted = sorted(cost_netprofit_list, key=itemgetter(2), reverse=True)
-#print(cost_netprofit_sorted)
-swap = True
-while swap:
-    swap = False
-    for i in range(0,len(cost_netprofit_list)-1):
-        if cost_netprofit_list[i+1][2] > cost_netprofit_list[i][2]:
-            cost_netprofit_list[i], cost_netprofit_list[i+1] = cost_netprofit_list[i+1], cost_netprofit_list[i]
-            swap=True
+    for i in range(1, len(shares_list)+1):
+        for combination in combinations(shares_list, i):
+            combination_list.append(combination)
 
-"""Additionner les coûts des actions les plus profitable sans dépasser 500€"""
-
-stockscost = 0
-"""while stockscost < 501:
-
-    a = list(accumulate(cost_netprofit_sorted[2]))
-
-    for i in cost_netprofit_sorted:
-        stockscost += i[2]
-        #print(stockscost)"""
+    return combination_list
 
 
+def combination_price(combination_list):
+    '''Calcul le prix de chaque combinaison'''
+
+    combination_price_list = []
+    
+    for combination in combination_list:
+        combination_price = 0       
+        for price in combination:    
+            combination_price += price[1]
+        combination_price_list.append(combination_price)
+
+    return combination_price_list
 
 
-l = (2,8,4,6,3,5,1,7)
+def combination_profit(combination_list):
+    '''Calcul le bénéfice de chaque combinaison'''
+
+    combination_profit_list = []
+
+    for combination in combination_list:
+        combination_profit = 0        
+        for profit in combination:
+            combination_profit += profit[2]
+        combination_profit_list.append(combination_profit)
+
+    return combination_profit_list
+
+
+def combination_infos(combination_list, combination_price, combination_profit):
+    '''Créé une liste avec les combinations, le prix et le bénéfice de chacune d'elle'''
+
+    combination_infos = []
+
+    for combination, price, profit in zip(combination_list, combination_price, combination_profit):
+
+        combination_infos.append((combination, price, profit))
+    
+    return combination_infos
+
+
+        
+def best_result(combination_infos):
+    '''Récupère la meilleure combinaison en terme de bébéfice sans dépenser plus de 500€'''
+
+    price = 0
+    best_profit = 0
+    
+    for combination in combination_infos:
+        if combination[1] <= 500 and combination[2] > best_profit:
+            price = combination[1]
+            best_profit = combination[2]
+            best_combination = combination
+
+    best_shares = [share[0] for share in best_combination[0]]
+
+    return best_shares, price, best_profit
+
+def main():
+
+    share_list = shares()
+    combination = combination_list(share_list)
+    price = combination_price(combination)
+    profit = combination_profit(combination)
+    infos = combination_infos(combination, price, profit)
+    best_combination, best_combination_price, best_combination_profit = best_result(infos)
+
+    print(f"Le meilleur investissement est la combinaison d'actions : {best_combination}")
+    print(f"Avec un bénéfice de : {best_combination_profit}€ après 2 ans")
+    print(f"Pour un coût d'achat total de: {best_combination_price}€")
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print(f'---{time.time()-start_time} secondes---')
