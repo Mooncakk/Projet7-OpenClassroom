1025d81a6a177541512a799c3abfc52e25ff497b
Adm1n <adm1n@Kal1X.lan>
2023-10-09 00:26:45 +0200

    update
    Suppression fonction combination_infos

--- bruteforce.py
+++ bruteforce.py
@@ -3,19 +3,12 @@ from itertools import combinations
 import time
 
 def shares(data='./Shares.csv'):
-    '''Lis le fichier csv et créé un tuples contenant les infos des actions, les prix et le bénéfice apres 2 ans''' 
+    '''Lis le fichier csv et créé une liste contenant les infos des actions, les prix, les bénéfices apres 2 ans'''
 
     data = pd.read_csv(data, delimiter = ';')
-    shares_list = []
+    data["Profit"] = data["Cout"] * data["Bénéfice"] / 100
+    shares_list = data.values
 
-    for row in data.itertuples():
-        share = row[1]
-        price = row[2]
-        efficiency = row[3]
-        profit = price * efficiency/100
-        share_infos = share, price, profit
-        shares_list.append(share_infos)
-               
     return shares_list
 
 def combination_list(shares_list):
@@ -34,7 +27,7 @@ def combination_price(combination_list):
     '''Calcul le prix de chaque combinaison'''
 
     combination_price_list = []
-    
+
     for combination in combination_list:
         combination_price = 0       
         for price in combination:    
@@ -52,56 +45,40 @@ def combination_profit(combination_list):
     for combination in combination_list:
         combination_profit = 0        
         for profit in combination:
-            combination_profit += profit[2]
+            combination_profit += profit[3]
         combination_profit_list.append(combination_profit)
 
     return combination_profit_list
 
-
-def combination_infos(combination_list, combination_price, combination_profit):
-    '''Créé une liste avec les combinations, le prix et le bénéfice de chacune d'elle'''
-
-    combination_infos = []
-
-    for combination, price, profit in zip(combination_list, combination_price, combination_profit):
-
-        combination_infos.append((combination, price, profit))
-    
-    return combination_infos
-
-
-        
-def best_result(combination_infos):
+def best_result(combination_list, combination_price, combination_profit):
     '''Récupère la meilleure combinaison en terme de bébéfice sans dépenser plus de 500€'''
 
     price = 0
     best_profit = 0
     
-    for combination in combination_infos:
-        if combination[1] <= 500 and combination[2] > best_profit:
-            price = combination[1]
-            best_profit = combination[2]
+    for i, combination in enumerate(combination_list):
+
+        if combination_price[i] <= 500 and combination_profit[i] > best_profit:
             best_combination = combination
+            price = combination_price[i]
+            best_profit = combination_profit[i]
 
-    best_shares = [share[0] for share in best_combination[0]]
+    best_shares = [share[0] for share in best_combination]
 
-    return best_shares, price, best_profit
 
-def main():
+    return best_shares, price, round(best_profit,2)
+
+if __name__ == '__main__':
+    start_time = time.time()
 
     share_list = shares()
     combination = combination_list(share_list)
     price = combination_price(combination)
     profit = combination_profit(combination)
-    infos = combination_infos(combination, price, profit)
-    best_combination, best_combination_price, best_combination_profit = best_result(infos)
+    best_combination, best_combination_price, best_combination_profit = best_result(combination, price, profit)
+    best_combination = ', '.join(best_combination)
 
     print(f"Le meilleur investissement est la combinaison d'actions : {best_combination}")
     print(f"Avec un bénéfice de : {best_combination_profit}€ après 2 ans")
     print(f"Pour un coût d'achat total de: {best_combination_price}€")
-
-
-if __name__ == '__main__':
-    start_time = time.time()
-    main()
     print(f'---{time.time()-start_time} secondes---')
